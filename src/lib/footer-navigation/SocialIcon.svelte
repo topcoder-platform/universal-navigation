3622ca8737a42a58f43c6b18e0bc581f559fb734
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-11-21 22:17:26 +0200

    Footer stub

---
+++ src/lib/footer-navigation/SocialIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { getPublicPath } from "lib/utils/paths";
+  import styles from './SocialIcon.module.scss';
+
+  export let icon: string;
+  export let href: string;
+  export let alt: string;
+
+  const iconImg = getPublicPath(`/assets/${icon}`);
+</script>
+
+<span class={styles.iconWrap}>
+  <a href={href} target="_blank" rel="noreferrer">
+    <img src={iconImg} alt={alt} />
+  </a>
+</span>
