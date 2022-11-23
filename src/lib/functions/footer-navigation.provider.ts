79c03786f9ae5fa0948d854979a9f34a81b669b8
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-11-23 21:21:37 +0200

    set default host for footer

--- src/lib/functions/footer-navigation.provider.ts
+++ src/lib/functions/footer-navigation.provider.ts
@@ -1,8 +1,12 @@
 import navMenuJson from 'assets/footer-navigation.json';
+import { DEFAULT_HOST_URL } from 'lib/config';
 import { parseMenuItem } from "lib/utils/navigation-menu"
 import type { NavMenuItem } from './nav-menu-item.model';
 
-const navMenu = parseMenuItem(navMenuJson.defaultHost, navMenuJson as unknown as NavMenuItem)
+const navMenu = parseMenuItem(
+  DEFAULT_HOST_URL,
+  navMenuJson as unknown as NavMenuItem
+)
 
 export function getFooterNavItems(): NavMenuItem[] {
   return navMenu.children
