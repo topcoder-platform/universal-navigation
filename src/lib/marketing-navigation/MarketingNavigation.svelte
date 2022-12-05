69c83a7b1fc2d6a86051828ea6851a8cdf563ead
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-12-05 14:58:02 +0200

    Remove marketing additional logo icon

--- src/lib/marketing-navigation/MarketingNavigation.svelte
+++ src/lib/marketing-navigation/MarketingNavigation.svelte
@@ -5,7 +5,6 @@
   */
   import type { NavMenuItem } from 'lib/functions/nav-menu-item.model';
   import { getActiveRoute, getMainNavItems } from 'lib/functions/marketing-navigation.provider'
-  import TcLogo from 'lib/components/TcLogo.svelte';
   import UserArea from 'lib/components/UserArea.svelte';
   import NavigationBar from './components/NavigationBar.svelte';
   import { isMobile } from 'lib/utils/window-size.store';
@@ -22,7 +21,6 @@
     menuItems={menuItems}
     isMobile={$isMobile}
   >
-    <TcLogo slot="logo" />
     <UserArea slot="auth" />
   </NavigationBar>
 
