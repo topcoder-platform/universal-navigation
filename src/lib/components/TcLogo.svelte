9af2c58c60185e3b623cf0263ec464456c36d817
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-12-08 17:39:31 +0200

    Fix navigation width on larger scresn according to latest figma

--- src/lib/components/TcLogo.svelte
+++ src/lib/components/TcLogo.svelte
@@ -2,10 +2,11 @@
   import { DEFAULT_HOST_URL } from 'lib/config';
   import { getPublicPath } from 'lib/utils/paths';
   import styles from './TcLogo.module.scss';
-  
+
   export let minVersion: boolean = false;
-  
-  const imgUrl = getPublicPath(`/assets/logo${minVersion ? '.min' : ''}.svg`);
+
+  let imgUrl: string;
+  $: imgUrl = getPublicPath(`/assets/logo${minVersion ? '.min' : ''}.svg`);
 </script>
 
 <a class={styles.logo} href={DEFAULT_HOST_URL} target="_top">
