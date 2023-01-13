1f35551050d987eb299fb7d7245aa0308de8e5d8
Brooke <brooke.souza@topcoder.com>
2023-01-13 13:51:55 -0800

    PROD-3669 remove debugging code

--- src/lib/marketing-navigation/components/AboutUsMenuItem.svelte
+++ src/lib/marketing-navigation/components/AboutUsMenuItem.svelte
@@ -2,9 +2,6 @@
   import LinksMenu from "lib/components/LinksMenu.svelte";
   import { navItems } from "lib/config/nav-menu/nav-items.config";
   import { isMobile } from 'lib/utils/window-size.store';
-    import { is_promise } from "svelte/internal";
-
-  console.debug($isMobile)
 </script>
 
 {#if !$isMobile}
