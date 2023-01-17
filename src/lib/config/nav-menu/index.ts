44ba52bb28e02369f3945ff9e73e5b70053e1aac
Brooke <brooke.souza@topcoder.com>
2023-01-17 15:50:52 -0800

    Minor Fixes

--- src/lib/config/nav-menu/index.ts
+++ src/lib/config/nav-menu/index.ts
@@ -14,8 +14,8 @@ function getEnvValue(viteKey: string): string {
 }
 
 export const CHALLENGE_HOST: string = getEnvValue('VITE_CHALLENGE_HOST')
-export const COMMUNITY_HOST: string = import.meta.env.VITE_COMMUNITY_HOST ?? 'https://community.topcoder.com'
-export const FORUM_HOST: string = import.meta.env.VITE_FORUM_HOST ?? 'https://discussions.topcoder.com'
-export const ONLINE_REVIEW_HOST: string = import.meta.env.VITE_ONLINE_REVIEW_HOST ?? 'https://software.topcoder.com'
-export const PLATFORM_UI_HOST: string = import.meta.env.VITE_PLATFORM_UI_HOST ?? 'https://platform-ui.topcoder.com'
-export const TC_API_V5_HOST: string = import.meta.env.VITE_TC_API_V5_HOST ?? 'https://api.topcoder.com/v5'
+export const COMMUNITY_HOST: string = getEnvValue('VITE_COMMUNITY_HOST')
+export const FORUM_HOST: string = getEnvValue('VITE_FORUM_HOST')
+export const ONLINE_REVIEW_HOST: string = getEnvValue('VITE_ONLINE_REVIEW_HOST')
+export const PLATFORM_UI_HOST: string = getEnvValue('VITE_PLATFORM_UI_HOST')
+export const TC_API_V5_HOST: string = getEnvValue('VITE_TC_API_V5_HOST')
