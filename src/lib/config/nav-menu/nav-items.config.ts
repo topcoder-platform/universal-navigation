85917c7571432b03898275cdfa52ae3d5e1a13c5
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-01-19 16:50:39 +0200

    PROD-3694 fix url for webside-design

--- src/lib/config/nav-menu/nav-items.config.ts
+++ src/lib/config/nav-menu/nav-items.config.ts
@@ -427,7 +427,7 @@ export const navItems: NavItemConfig = {
     websiteDesign: {
         label: 'Website Design',
         description: 'Work directly with the Topcoder community to design your website.',
-        url: getWordpressUrl('/customer/website-design'),
+        url: getWordpressUrl('/ui-ux-design/website-design'),
     },
     whyTopcoder: {
         label: 'Why Topcoder',
