32bc70c200810d9ee7673b8c506bca035e896618
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-02-08 17:20:46 +0200

    PROD-3299 - report a bug modal

---
+++ src/lib/components/modals/SubmitBugModal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import SupportModal from "./SupportModal.svelte";
+
+  export let isVisible = '';
+
+</script>
+
+<SupportModal
+  bind:isVisible={isVisible}
+  description='Please describe the bug'
+>
+  <svelte:fragment slot="description">
+    <p>
+      Oops you’ve found a bug!
+    </p>
+    <p>You can report the issue and our team will get it resolved.</p>
+  </svelte:fragment>
+</SupportModal>
