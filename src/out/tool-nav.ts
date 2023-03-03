0de8ec94c6d12b382028d0335f463fcfb804e1ba
Vasilica <olariu.vasilica@gmail.com>
2023-03-03 18:12:03 +0200

    remove 'out' folder

--- src/out/tool-nav.ts
+++
@@ -1,1 +0,0 @@
-export default import('../lib/tool-navigation/ToolNavigation.svelte').then(d => d.default)
--- types/src/out/footer-nav.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="svelte/types/runtime/ambient" />
-declare const _default: Promise<typeof import("*.svelte").default>;
-export default _default;
