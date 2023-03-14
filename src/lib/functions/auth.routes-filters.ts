f3af12375d043828915f978ab410aecfab19cab1
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-03-14 11:53:05 +0200

    UN-203 - render the whole menu when user has no specific role assigned

--- src/lib/functions/auth.routes-filters.ts
+++ src/lib/functions/auth.routes-filters.ts
@@ -1,6 +1,6 @@
 import { filterRoutes } from 'lib/utils/routes';
 import type { NavMenuItem } from './nav-menu-item.model';
-import { getJwtUserRoles } from './user-profile.provider';
+import { getUserAppRoles } from './user-profile.provider';
 
 /**
  * Filters the provided navigation menu items based on the user roles of the currently authenticated user
@@ -9,9 +9,9 @@ import { getJwtUserRoles } from './user-profile.provider';
  * @return {NavMenuItem} The filtered menu items based on the user roles
  */
 export const filterRoutesByUserRole = (routes: NavMenuItem) => {
-  const userRoles = getJwtUserRoles();
+  const userRoles = getUserAppRoles();
 
-  return filterRoutes(routes, (item: NavMenuItem) => !item.auth?.requireRoles || (
+  return filterRoutes(routes, (item: NavMenuItem) => !userRoles?.length || !item.auth?.requireRoles || (
     item.auth.requireRoles.some(role => userRoles.indexOf(role) > -1)
   ));
 }
