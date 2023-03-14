e82cfbebbebcb18e737bb2456385624f8ebbf69a
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-03-14 11:55:53 +0200

    update types

--- types/src/lib/config/auth.d.ts
+++ types/src/lib/config/auth.d.ts
@@ -2,6 +2,7 @@ export declare enum AUTH_USER_ROLE {
     talent = "Topcoder Talent",
     customer = "Topcoder Customer"
 }
+export declare const AUTH_USER_ROLE_VALUES: AUTH_USER_ROLE[];
 export declare const AUTH_TALENT_ROLES: AUTH_USER_ROLE[];
 export declare const AUTH_CUSTOMER_ROLES: AUTH_USER_ROLE[];
 export declare const TALENT_ROUTE_GUARD: {
