cc14fe8caec18331f1a1314182e740ad5fe9ed05
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-07-05 12:01:34 +0300

    MP-70 - hide the completeness nudges when user is customer

--- types/src/lib/functions/user-profile.provider.d.ts
+++ types/src/lib/functions/user-profile.provider.d.ts
@@ -1,8 +1,10 @@
 import type { AuthUser } from '../../main';
+import { AUTH_USER_ROLE } from '../config/auth';
 export declare type fetchUserProfileFn = () => AuthUser | null;
 export declare const getJwtUserhandle: () => AuthUser['handle'] | undefined;
 export declare const getJwtUserRoles: () => AuthUser['roles'] | undefined;
 export declare const getUserAppRoles: () => AuthUser['roles'] | undefined;
+export declare const checkUserAppRole: (role: AUTH_USER_ROLE) => boolean;
 /**
  * Fetches the user profile based on the handle that's stored in the jwt cookie
  * @returns Promise<AuthUser>
