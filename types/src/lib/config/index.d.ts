88b436cf307ec3b446729b4eaeae9fed7ee53542
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-10-02 18:37:50 +0300

    update types

--- types/src/lib/config/index.d.ts
+++ types/src/lib/config/index.d.ts
@@ -1,4 +1,5 @@
 export declare const CHAMELEON_KEY_ID: string;
 export declare const USERFLOW_ENVIRONMENT_TOKEN: string;
 export declare const USERFLOW_TC_SIGNATURE: string;
+export declare const USERFLOW_DEFAULT_SURVEY_DELAY: string;
 export * from './hosts';
