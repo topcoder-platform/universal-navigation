2a5be9591820613f8ff6452eb622701583ad822a
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-11-13 19:09:36 +0200

    Add config value for disabling userflow

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -5,6 +5,7 @@
 import { getEnvValue } from "./env-vars"
 
 export const CHAMELEON_KEY_ID: string = getEnvValue('VITE_CHAMELEON_KEY_ID', '')
+export const DISABLE_USERFLOW: string = getEnvValue('VITE_DISABLE_USERFLOW', '')
 export const USERFLOW_ENVIRONMENT_TOKEN: string = getEnvValue('VITE_USERFLOW_ENVIRONMENT_TOKEN')
 export const USERFLOW_TC_SIGNATURE: string = getEnvValue('VITE_USERFLOW_TC_SIGNATURE')
 export const USERFLOW_DEFAULT_SURVEY_DELAY: string = getEnvValue('VITE_USERFLOW_DEFAULT_SURVEY_DELAY', '3000')
