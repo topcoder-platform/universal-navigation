d838fc81324a703e62f73c1cf77159898af4f43c
Justin Gasper <jmgasper@gmail.com>
2024-05-03 08:45:12 +1000

    Add flag to disable profile nudges

--- src/lib/components/user-area/UserArea.svelte
+++ src/lib/components/user-area/UserArea.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { getAppContext } from 'lib/app-context';
-  import { checkUserAppRole, fetchUserProfile } from 'lib/functions/user-profile.provider';
-  import { fetchUserProfileCompletedness, dismissNudgesBasedOnHost } from 'lib/functions/profile-nudges';
-  import { AUTH_USER_ROLE } from 'lib/config/auth';
+  import { onMount } from "svelte";
+  import { getAppContext } from "lib/app-context";
+  import {
+    checkUserAppRole,
+    fetchUserProfile,
+  } from "lib/functions/user-profile.provider";
+  import {
+    fetchUserProfileCompletedness,
+    dismissNudgesBasedOnHost,
+  } from "lib/functions/profile-nudges";
+  import { AUTH_USER_ROLE } from "lib/config/auth";
+  import { DISABLE_NUDGES } from "lib/config/profile-toasts.config";
 
-  import ToolSelector from '../tool-selector/ToolSelector.svelte';
-  import Button from '../Button.svelte';
-  import VerticalSeparator from '../VerticalSeparator.svelte';
+  import ToolSelector from "../tool-selector/ToolSelector.svelte";
+  import Button from "../Button.svelte";
+  import VerticalSeparator from "../VerticalSeparator.svelte";
 
-  import UserAvatar from './UserAvatar.svelte';
-  import styles from './UserArea.module.scss'
-  import Completedness from './Completedness.svelte';
-  import SignupPopup from './SignupPopup.svelte';
-  import LoginPopup from './LoginPopup.svelte';
+  import UserAvatar from "./UserAvatar.svelte";
+  import styles from "./UserArea.module.scss";
+  import Completedness from "./Completedness.svelte";
+  import SignupPopup from "./SignupPopup.svelte";
+  import LoginPopup from "./LoginPopup.svelte";
 
   const ctx = getAppContext();
 
   // debounce updates to user if user.handle stays the same
-  let debounce = '';
+  let debounce = "";
 
   let signupBtnRef: HTMLElement;
   let loginBtnRef: HTMLElement;
@@ -33,28 +40,44 @@
 
   async function fetchProfileDetails() {
     // do nothing if user is not authenticated or has customer role
-    if (!user || checkUserAppRole(AUTH_USER_ROLE.customer) || debounce === user.handle) {
+    if (
+      !user ||
+      checkUserAppRole(AUTH_USER_ROLE.customer) ||
+      debounce === user.handle
+    ) {
       return;
     }
 
     debounce = user.handle;
-    const completednessData = await fetchUserProfileCompletedness(user, true);
 
-    if (!completednessData) {
-      return;
+    if (!DISABLE_NUDGES) {
+      const completednessData = await fetchUserProfileCompletedness(user, true);
+
+      if (!completednessData) {
+        return;
+      }
+      $ctx.auth = {
+        ...$ctx.auth,
+        profileCompletionData: {
+          completed: completednessData.data?.percentComplete === 100,
+          handle: completednessData.handle,
+          percentComplete: completednessData.data?.percentComplete,
+          showToast: completednessData.showToast,
+        },
+      };
+    } else {
+      $ctx.auth = {
+        ...$ctx.auth,
+        profileCompletionData: {
+          completed: true,
+          handle: user?.handle,
+          percentComplete: 100,
+          showToast: "",
+        },
+      };
     }
 
-    $ctx.auth = {
-      ...$ctx.auth,
-      profileCompletionData: {
-        completed: completednessData.data?.percentComplete === 100,
-        handle: completednessData.handle,
-        percentComplete: completednessData.data?.percentComplete,
-        showToast: completednessData.showToast,
-      },
-    };
-
-    setTimeout(() => debounce = '', 100);
+    setTimeout(() => (debounce = ""), 100);
   }
 
   $: isReady && user?.handle && fetchProfileDetails();
@@ -64,9 +87,9 @@
       return;
     }
 
-    $ctx.auth = {...$ctx.auth, ready: false};
+    $ctx.auth = { ...$ctx.auth, ready: false };
     const authUser = await fetchUserProfile();
-    $ctx.auth = {...$ctx.auth, ready: true, user: authUser};
+    $ctx.auth = { ...$ctx.auth, ready: true, user: authUser };
   });
 </script>
 
@@ -74,33 +97,28 @@
   <VerticalSeparator />
   <div class={styles.userAreaWrap}>
     {#if !user}
-    <div class={styles.btnsWrap}>
-      <div>
-        <Button label="Log In" bind:ref={loginBtnRef} />
-        <LoginPopup targetEl={loginBtnRef} />
-      </div>
+      <div class={styles.btnsWrap}>
+        <div>
+          <Button label="Log In" bind:ref={loginBtnRef} />
+          <LoginPopup targetEl={loginBtnRef} />
+        </div>
 
-      <div>
-        <Button
-          variant="primary"
-          label="Sign Up"
-          bind:ref={signupBtnRef}
-        />
-        <SignupPopup targetEl={signupBtnRef} />
+        <div>
+          <Button variant="primary" label="Sign Up" bind:ref={signupBtnRef} />
+          <SignupPopup targetEl={signupBtnRef} />
+        </div>
       </div>
-    </div>
-    {:else }
+    {:else}
       <ToolSelector />
       <UserAvatar
-        user={user}
-        onSignOut={onSignOut}
+        {user}
+        {onSignOut}
         profileCompletionPerc={profileCompletionData?.percentComplete}
       >
-        {#if profileCompletionData}
-        <Completedness />
+        {#if profileCompletionData && !!DISABLE_NUDGES}
+          <Completedness />
         {/if}
       </UserAvatar>
     {/if}
   </div>
 {/if}
-
