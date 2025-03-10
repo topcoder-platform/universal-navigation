cebe08114a6c944bf9f5476c425f9ee36920ba7f
Vasilica Olariu <olariu.vasilica@gmail.com>
2025-03-10 17:12:27 +0200

    PM-908 - update uninav community

--- src/lib/config/nav-menu/main-navigation.config.ts
+++ src/lib/config/nav-menu/main-navigation.config.ts
@@ -6,4 +6,14 @@ export const mainNavigationItems: NavMenuItem[] = [
   allNavItems.howItWorks,
   allNavItems.talent,
   allNavItems.customerStories,
+  allNavItems.freelancer,
+  {
+    ...allNavItems.community,
+    children: [
+      allNavItems.opportunities,
+      allNavItems.mmTournament,
+      allNavItems.aiHub,
+      allNavItems.articles,
+    ]
+  },
 ]
