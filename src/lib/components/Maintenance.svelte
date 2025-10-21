12f3357a4032bcd8bfc21ad03b0c67bf79609c33
Vasilica Olariu <olariu.vasilica@gmail.com>
2025-10-21 13:54:31 +0300

    Update wallet maintenance window text

--- src/lib/components/Maintenance.svelte
+++ src/lib/components/Maintenance.svelte
@@ -6,7 +6,7 @@
 <div class={styles.banner}>
   <div class={styles.bannerContent}>
     <div class={styles.bannerText}>
-      Topcoder will be undergoing maintenance on Tuesday, October 21, 2025 from 1 AM to 6 AM EST
+      Wallet application will remain under maintenance until 11:30 PM EST on Tuesday, October 21, 2025.
     </div>
   </div>
 </div>
