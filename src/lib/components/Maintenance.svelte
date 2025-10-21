964ba2d71f420f08e895846267ddbd6b3e2651f0
Vasilica Olariu <olariu.vasilica@gmail.com>
2025-10-21 14:05:36 +0300

    update maintenance text

--- src/lib/components/Maintenance.svelte
+++ src/lib/components/Maintenance.svelte
@@ -6,7 +6,7 @@
 <div class={styles.banner}>
   <div class={styles.bannerContent}>
     <div class={styles.bannerText}>
-      Wallet application will remain under maintenance until 11:30 PM EST on Tuesday, October 21, 2025.
+      Wallet application will be under maintenance on Tuesday, October 21, 2025
     </div>
   </div>
 </div>
