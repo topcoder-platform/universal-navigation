318ea0f15483debf9e405571c83f5b4d6b28b8c9
Kiril Kartunov <kkartunov@users.noreply.github.com>
2025-11-02 15:04:41 +0200

    Revert "Maintenance banner 11/02"

--- src/lib/components/Maintenance.svelte
+++ src/lib/components/Maintenance.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import styles from './Maintenance.module.scss';
+  const bannerName = 'maintenance';
 </script>
 
 <div class={styles.banner}>
   <div class={styles.bannerContent}>
     <div class={styles.bannerText}>
-      The Topcoder platform will undergo scheduled maintenance on November 2, 2025. We apologize for any inconvenience this may cause.
+      Wallet application will be under maintenance on Tuesday, October 21, 2025
     </div>
   </div>
 </div>
